70486af69b844f9d5c43a909a450b51b9d3fb731
Quality0610 <kzktcy0610@gmail.com>
2022-11-15 23:52:27 +0900

    https://www.youtube.com/watch?v=YbsBQbqxy1Iの学習

---
+++ mycalc/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculater from './component/Calculater';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculater/>
+    </div>
+  );
+}
+
+export default App;
---
+++ mycalc/src/component/Calculater.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { calculate, State } from "../logic/calculate";
+import ButtonPanel from "./ButtonPanel";
+import Display from "./Display";
+
+export default function Calculater(){
+    const [state, setState] = useState<State>({
+        current: "0",
+        operand: 0,
+        operator: null,
+        isNextClear: false
+    });
+
+    const buttonHandler = (code: string) => {
+        const nextState = calculate(code, state)
+        setState(nextState);
+    }
+    return <div>
+        <Display/>
+        <ButtonPanel buttonHandler={buttonHandler}/>
+    </div>
+}
